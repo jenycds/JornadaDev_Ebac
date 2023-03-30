b70d3010b4c313d2257ea18a18392165fe445bfd
Jenyffer <123900745+jenycds@users.noreply.github.com>
2023-03-30 17:52:28 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Videos from './pages/Videos';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app_videos'>
+      <Videos />
+      <Videos />
+      <Videos />
+      <Videos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
